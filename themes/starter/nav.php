6e65f1ec3ddc61bbbed8dba98787bfa22fbd8d71
shaun <shaungumbaketi@gmail.com>
2020-05-30 13:29:58 +1000

    fixed the nav bar

--- themes/starter/nav.php
+++ themes/starter/nav.php
@@ -1,6 +1,6 @@
 <html>
 <div id="nav-container">
-    <div id="page" class="site">
+    <div id="page" class="site-nav-container">
         <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'townsville-jazz-club' ); ?></a>
 
         <header id="masthead" class="site-header">
@@ -10,10 +10,10 @@
                 <button class="menu-toggle" aria-controls="primary-menu"
                         aria-expanded="false"><?php esc_html_e('Primary Menu', 'townsville-jazz-club'); ?></button>
                 <?php echo wp_nav_menu(); ?>
-
+                <?php include "media_player.php"; ?>
             </nav><!-- #site-navigation -->
         </header>
-        <?php include "media_player.php"; ?>
+
     </div>
 </div>
 </html>
