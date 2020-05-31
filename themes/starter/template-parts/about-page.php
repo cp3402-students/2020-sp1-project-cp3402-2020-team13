e40bb7df74dc15530987ff231d9d4460e18aff76
ryanclark532 <42229549+ryanclark532@users.noreply.github.com>
2020-05-31 20:01:50 +1000

    Refactored the About Us Page
    Formatted it to use a retating slide show dynamically populated with images from posts categorized 'bands'. Added a paraprased version of the clubs history

--- themes/starter/template-parts/about-page.php
+++ themes/starter/template-parts/about-page.php
@@ -8,71 +8,59 @@
  * @Template name: About_Pagee
  */
 get_header();
+$args = array(
+    'category' => '2',
+);
+$posts = get_posts($args);
 ?>
-<htm>
-    <head>
+<html>
+<div id="slideshow">
+    <div class="slide-wrapper">
+        <?php foreach ($posts as $post) {
+            $title = get_the_title($post);
+            $image = get_the_post_thumbnail_url($post);
+            $link = get_permalink($post);
+            echo "<a href=$link><div class=\"slide\" style='background-image: url($image);background-repeat: no-repeat;background-size: cover;'><h1 class=\"slide-number\">$title</h1></div></a>";
+        }
+        ?>
 
-    </head>
-    <body>
-
-
-<div class="about_page_container">
-
-
-<div class="about_page_text_container">
-
-
-<h1>Townsville Jazz Club</h1>
-<p>The Townsville Jazz Club has a long history of bringing together
-  talented musicians from the area to provide entertainment for the
-  Jazz loving people of Townsville. The club was founded in the early
-  90’s under the name of North Queensland Jazz Club before being changed
-  to its current name. However, what hasn’t changed is the clubs love for
-  Jazz, shown through the many performances the bands put on. </p>
+    </div>
 </div>
-
-<div class="about_page_text_container">
-  <h1>Our Bands</h1>
-  <p>The Club features the following Bands with their own unique style
-    of play. The bands consist of Townsville's finest Jazz musicians who
-    have years of performance experience. Check out the various bands below </p>
-
+<div class="bands-header">
+    <h1>Our Performers</h1>
+    <p>We at Townsville Jazz Club pride ourselves on having world class performers. We feature a range of very talented
+        local bands playing great jazz music in a relaxed yet vibrant environment.<br><br>
+        Our performers are all longtime members of the local jazz community, with some going back to as far as 1974!
+        Band members are common place a Jazz Club events, performing our not.
+    </p>
 </div>
-<div class="history_sub_pages_container" >
+<div class="history-header">
+    <h1>Jazz Club History</h1>
+    <h3>Harbour End Jazz Club</h3>
+    <p>Whilst jazz music had been played in Townsville long before the Harbour End Jazz Club was formed in 1980,
+        there had not been a recognised venue where jazz was performed for about twenty years.
+        This club effectively started the revival of the jazz scene in Townsville which was then carried on with
+        the formation of the North Queensland Jazz Club (NQ Jazz Club Inc. from 1995) and its subsequent change of name to Townsville Jazz Club Inc. in 2012.
+        The Harbour End Jazz Club was started in 1980 by Mo Murakami, Ted Redknap, Bob Hebden and Les Nicholson.
+        Ted and Mo were members of the widely acclaimed North Qld Area Army Band.  The Army band had a close association
+        with the local musicians and having Ted and Mo as Directors cemented that close relationship.
+        Mo also had an input into starting the NQ Jazz Club which still exists today and is a significant institution in the promotion of jazz in Townsville.  </p>
+    <h3>NQ Jazz Club INC.</h3>
+    <p>The formation of the North Queensland Jazz Club began with discussions led by Brian Linforth, Mo Murakami and Andrew Dajski around 1993.
+        By 1994 these guys were playing once a month for ‘beer money’ just to get the club started however the club Newsletter from 1999 states that on Sunday
+        11th April the club celebrated its fourth birthday and indicates the official start of North Queensland Jazz Club being 7th April 1995.</p>
+    <h3>Townsville Jazz Club INC.</h3>
+    <p>The Townsville Jazz Club was originally established as the NQ Jazz Club and operated under this name for approximately 18 years when the management committee began to
+        question whether the regional name of ‘North Queensland’ that did not clearly inform people where the jazz club was actually based was not assisting new people to locate the club. <br><br>
 
-    <?php
-
-      include 'band-sub-page-generator.php';
-
-     ?>
-
-
+        In this age of electronic searches to find recreational activities, it was felt that most new residents and people travelling through Townsville who were searching for jazz venues would
+        include jazz and Townsville in their search regardless of any other keywords that they may use in their search.  </p>
+    <h3>2012 - A year of growth and change</h3>
+    <p>
+        On Sunday 24th June 2012 more than half the club’s members were present for the launch of the new Townsville Jazz Club Inc. logo featuring Neville Minon.
+        This was a memorable night when we paid tribute to Nev for his 62 years of service to both jazz and brass band music.  With such a distinguished musical career
+        I don’t think anyone would begrudge Nev the honour of gracing the Townsville Jazz Club logo.  It was clear that Nev was very touched by the occasion and he and
+        his wife Christine were delighted with the image that was created through Bob Spillane’s photography and the artistry of Katrina Cataldo at Townprint.
+    </p>
 </div>
-
-  </div>
-
-  <div class="about_page_text_container">
-
-
-    <h1>Our History</h1>
-    <p>Townsville Jazz Club has a very long history. The club is proud of how far
-      they have come and how they are trying to spread the love of Jazz to the
-       next generation to continue the club's legacy. The following articles
-        provide an insight to the club's history. </p>
-
-
-        <div class="history_sub_pages_container" >
-            <div class="bands_sub_pages_container" >  </div>
-            <div class="bands_sub_pages_container" >  </div>
-            <div class="bands_sub_pages_container" >  </div>
-            <div class="bands_sub_pages_container" >  </div>
-            <div class="bands_sub_pages_container" >  </div>
-            <div class="bands_sub_pages_container" >  </div>
-
-          </div>
-      </div>
-
-
-</div>
-    </body>
-</htm>
+</html>
